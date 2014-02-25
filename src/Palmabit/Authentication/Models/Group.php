ae2d8c5b7198078d8446a65b98bef7491bbc8e38
Jacopo <beschi.jacopo@gmail.com>
2014-02-25 09:29:35 +0100

    fix test in ram

--- src/Palmabit/Authentication/Models/Group.php
+++ src/Palmabit/Authentication/Models/Group.php
@@ -5,19 +5,14 @@
  * @author jacopo beschi jacopo@jacopobeschi.com
  */
 use Cartalyst\Sentry\Groups\Eloquent\Group as SentryGroup;
+use Palmabit\Authentication\Traits\OverrideConnectionTrait;
 
 class Group extends SentryGroup
 {
+    use OverrideConnectionTrait;
+
     protected $guarded = ["id"];
 
     protected $fillable = ["name", "permissions", "editable"];
 
-    /**
-     * @override
-     * @return \Illuminate\Database\Connection
-     */
-    public function getConnection()
-    {
-        return static::resolveConnection('authentication');
-    }
 } 
