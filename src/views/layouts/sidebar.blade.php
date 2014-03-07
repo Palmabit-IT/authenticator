5ac7a213ffd6a4c4bcf1a1409acfe8ffe979cba7
Francesco <f.falanga@palmabit.com>
2014-03-07 17:32:44 +0100

    fix sticky footer

--- src/views/layouts/sidebar.blade.php
+++ src/views/layouts/sidebar.blade.php
@@ -1,8 +1,7 @@
 <ul class="nav nav-pills nav-stacked">
 @if(isset($sidebar_items) && $sidebar_items)
 @foreach($sidebar_items as $name => $link)
-	<?php dd($sidebar_items) ?>
-    <li class="{{Palmabit\Library\Views\Helper::get_active($link[0])}}">{{$link[1]}} <a href="{{$link}}">{{$name}}</a></li>
+    <li class="{{Palmabit\Library\Views\Helper::get_active($link)}}"> <a href="{{$link}}">{{$name}}</a></li>
 @endforeach
 @endif
 </ul>
