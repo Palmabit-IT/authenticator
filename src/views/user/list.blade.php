7b6a60494dae204088a51796326189d31eb1d650
Jacopo <beschi.jacopo@gmail.com>
2014-03-07 14:14:07 +0100

    fix user profile

--- src/views/user/list.blade.php
+++ src/views/user/list.blade.php
@@ -1,7 +1,7 @@
 @extends('authentication::layouts.base-2cols')
 
 @section('title')
-    Admin area: lista utenti
+Admin area: lista utenti
 @stop
 
 @section('content')
@@ -20,39 +20,39 @@
     @endif
     <h3>Utenti</h3>
     @if(! $users->isEmpty() )
-        <ul class="list-group">
+    <ul class="list-group">
         @foreach($users as $user)
-            <li class="list-group-item">
-                <span class="badge {{$user->activated ? 'badge-green' : 'badge-red'}}">&nbsp;</span>&nbsp;&nbsp;
-                {{$user->email}} - {{ucfirst($user->first_name)}} {{ucfirst($user->last_name)}}
-                @if(! $user->blocked)
-                <div class="pull-right">
-                    <a href="{{URL::action('Palmabit\Authentication\Controllers\UserController@editProfile', ['user_id' => $user->id])}}">
-                        <span class="glyphicon glyphicon-user">profilo</span>
-                    </a>&nbsp;
-                    <a href="{{URL::action('Palmabit\Authentication\Controllers\UserController@editUser', ['id' => $user->id])}}">
-                        <span class="glyphicon glyphicon-edit">modifica</span>
-                    </a>&nbsp;
-                    <a href="{{URL::action('Palmabit\Authentication\Controllers\UserController@deleteUser',['id' => $user->id, '_token' => csrf_token()])}}" >
-                        <span class="glyphicon glyphicon-trash margin-left-5 delete">cancella</span>
-                    </a>&nbsp;
-                </div>
-                @endif
-                <span class="clearfix"></span>
-            </li>
-            @endforeach
-        </ul>
+        <li class="list-group-item">
+            <span class="badge {{$user->activated ? 'badge-green' : 'badge-red'}}">&nbsp;</span>&nbsp;&nbsp;
+            {{$user->email}} - {{($user->user_profile()->count()) ? ucfirst($user->user_profile()->first()->first_name): ''}} {{($user->user_profile()->count()) ? ucfirst($user->user_profile()->first()->last_name): ''}}
+            @if(! $user->blocked)
+            <div class="pull-right">
+                <a href="{{URL::action('Palmabit\Authentication\Controllers\UserController@editProfile', ['user_id' => $user->id])}}">
+                    <span class="glyphicon glyphicon-user">profilo</span>
+                </a>&nbsp;
+                <a href="{{URL::action('Palmabit\Authentication\Controllers\UserController@editUser', ['id' => $user->id])}}">
+                    <span class="glyphicon glyphicon-edit">modifica</span>
+                </a>&nbsp;
+                <a href="{{URL::action('Palmabit\Authentication\Controllers\UserController@deleteUser',['id' => $user->id, '_token' => csrf_token()])}}" >
+                    <span class="glyphicon glyphicon-trash margin-left-5 delete">cancella</span>
+                </a>&nbsp;
+            </div>
+            @endif
+            <span class="clearfix"></span>
+        </li>
+        @endforeach
+    </ul>
     @else
-        <h5>Non ci sono utenti presenti nel sistema.</h5>
+    <h5>Non ci sono utenti presenti nel sistema.</h5>
     @endif
     <a href="{{URL::action('Palmabit\Authentication\Controllers\UserController@editUser')}}" class="btn btn-primary pull-right">Aggiungi</a>
 </div>
 @stop
 
 @section('footer_scripts')
-    <script>
-        $(".delete").click(function(){
-            return confirm("Sei sicuro di volere eliminare l'elemento selezionato?");
-        });
-    </script>
+<script>
+    $(".delete").click(function(){
+        return confirm("Sei sicuro di volere eliminare l'elemento selezionato?");
+    });
+</script>
 @stop
