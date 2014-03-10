3e718420ebf37377834e2c36ef4da3512d96d7af
Francesco <f.falanga@palmabit.com>
2014-03-10 17:27:54 +0100

    menù3

--- src/composers.php
+++ src/composers.php
@@ -21,12 +21,12 @@ View::composer('authentication::layouts.*', function ($view){
  */
 View::composer(['authentication::user.*', 'authentication::group.*', 'authentication::permission.*'], function ($view){
     $view->with('sidebar_items', [
-                                    "Lista utenti" => array( URL::route('users.list'), "<i class='glyphicon glyphicon-th-list'></i>"),
-                                    "Aggiungi utente" => array( URL::route('users.edit'), "<i class='glyphicon glyphicon-plus'></i>"),
-                                    "Lista gruppi" => array( URL::route('users.groups.list'), "<i class='glyphicon glyphicon-th-list'></i>"),
-                                    "Aggiungi gruppo" => array( URL::route('users.groups.edit'), "<i class='glyphicon glyphicon-plus'></i>"),
-                                    "Lista permessi" => array( URL::route('users.permission.list'), "<i class='glyphicon glyphicon-th-list'></i>"),
-                                    "Aggiungi permesso" => array( URL::route('users.permission.edit'), "<i class='glyphicon glyphicon-plus'></i>"),
+                                    "Lista utenti" =>  URL::route('users.list'),
+                                    "Aggiungi utente" =>  URL::route('users.edit'),
+                                    //"Lista gruppi" => array( URL::route('users.groups.list'), "<i class='glyphicon glyphicon-th-list'></i>"),
+                                    //"Aggiungi gruppo" => array( URL::route('users.groups.edit'), "<i class='glyphicon glyphicon-plus'></i>"),
+                                    //"Lista permessi" => array( URL::route('users.permission.list'), "<i class='glyphicon glyphicon-th-list'></i>"),
+                                    //"Aggiungi permesso" => array( URL::route('users.permission.edit'), "<i class='glyphicon glyphicon-plus'></i>"),
                                  ]);
 });
 
