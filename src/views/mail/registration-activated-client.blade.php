79b6cf550b1b8c7f231e12085e7ea67499523a7d
Francesco <f.falanga@palmabit.com>
2014-03-18 22:36:06 +0100

    email translation for auth

--- src/views/mail/registration-activated-client.blade.php
+++ src/views/mail/registration-activated-client.blade.php
@@ -4,13 +4,13 @@
     <meta charset="utf-8">
 </head>
 <body>
-<h2>Benvenuto su {{Config::get('authentication::app_name')}}</h2>
+<h2>{{L::t('Welcome to')}} {{Config::get('authentication::app_name')}}</h2>
 <div>
-    Buongiorno {{ $body['email'] }}
-    <strong>Il tuo utente è stato attivato.</strong>
+    {{L::t('Goodmorning')}} {{ $body['email'] }}
+    <strong>{{L::t('Your username has been activated')}}.</strong>
     <br/>
-    <strong>Puoi effettuare il login al nostro sito usando l'email {{ $body['email']}} e la password da te inserita.</strong>
-    <a href="{{URL::to('/')}}" target="_blank">Vai al sito</a>
+    <strong>{{L::t('Login with email and password you selected')}}.</strong>
+    <a href="{{URL::to('/')}}" target="_blank">{{L::t('Go shopping')}}!</a>
 </div>
 </body>
 </html>
--- src/views/mail/registration-waiting-client.blade.php
+++ src/views/mail/registration-waiting-client.blade.php
@@ -4,11 +4,11 @@
     <meta charset="utf-8">
 </head>
 <body>
-<h2>Richiesta di registrazione su {{Config::get('authentication::app_name')}}</h2>
+<h2>{{L::t('Application for')}} {{Config::get('authentication::app_name')}}</h2>
 <div>
-    <strong>La richiesta di registrazione è stata inoltrata con successo. Un moderatore validerà i dati da te inseriti</strong>
+    <strong>{{L::t('The request for registration has been submitted successfully. A moderator will validate the data you have entered')}}</strong>
     <br/>
-    <strong>Riepilogo dati: </strong>
+    <strong>{{L::t('Summary data')}}: </strong>
     <ul>
         <li>Username: {{$body['email']}}</li>
         <li>Password: {{$body['password']}}</li>
