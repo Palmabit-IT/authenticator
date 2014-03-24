3fe5e28474d31021e1135b30c2231d1f92b41a91
Francesco <f.falanga@palmabit.com>
2014-03-24 16:37:52 +0100

    update change password

--- src/views/auth/changepassword.blade.php
+++ src/views/auth/changepassword.blade.php
@@ -1,19 +1,40 @@
-@extends('authentication::layouts.baseauth')
-@section('container')
-    <h1>Modifica password</h1>
-    <?php $message = Session::get('message'); ?>
-    @if( isset($message) )
-        <div class="alert alert-success">{{$message}}</div>
-    @endif
-    @if($errors && ! $errors->isEmpty() )
-        @foreach($errors->all() as $error)
-            <div class="alert alert-danger">{{$error}}</div>
-        @endforeach
-    @endif
-    {{Form::open(array('url' => URL::action("Palmabit\Authentication\Controllers\AuthController@postChangePassword"), 'method' => 'post') )}}
-            {{FormField::password(array('label' => 'password' ))}}
-            {{Form::hidden('email',$email)}}
-            {{Form::hidden('token',$token)}}
-            {{Form::submit('Cambia', array("class"=>"btn btn-large btn-primary"))}}
-    {{Form::close()}}
+@extends('layouts.nobar')
+@section('content')
+
+  <div class="container">
+      <div class="row">
+          <div class="col-md-4 col-md-offset-4">
+                <div class="login-panel panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Modifica password</h3>
+                    </div>
+                    <div class="panel-body">
+                        <?php $message = Session::get('message'); ?>
+                        @if( isset($message) )
+                            <div class="alert alert-success">{{$message}}</div>
+                        @endif
+                        @if($errors && ! $errors->isEmpty() )
+                            @foreach($errors->all() as $error)
+                                <div class="alert alert-danger">{{$error}}</div>
+                            @endforeach
+                        @endif
+                        {{Form::open(array('url' => URL::action("Palmabit\Authentication\Controllers\AuthController@postChangePassword"), 'method' => 'post') )}}
+                            <fieldset>
+                            <div class="form-group">
+                                  <input class="form-control" placeholder="Nuova password" name="password" type="text" autofocus>
+                              </div>
+                            {{Form::hidden('email',$email)}}
+                            {{Form::hidden('token',$token)}}
+                            {{Form::submit('Cambia', array("class"=>"btn btn-lg btn-success btn-block"))}}
+                            </fieldset>
+                        {{Form::close()}}
+                    </div>
+                </div>
+                <p>
+                    <a href="http://www.fism.net" alt="Torna al sito">Torna al sito</a>
+                </p>
+          </div>
+      </div>
+  </div>
+
 @stop
