fbd25ae6512bb76e75cf6fa0abf3885854941345
Jacopo <beschi.jacopo@gmail.com>
2014-03-25 14:36:32 +0100

    fix mail

--- src/Palmabit/Authentication/Validators/UserSignupValidator.php
+++ src/Palmabit/Authentication/Validators/UserSignupValidator.php
@@ -5,7 +5,7 @@ use Palmabit\Library\Validators\OverrideConnectionValidator;
 class UserSignupValidator extends OverrideConnectionValidator
 {
     protected static $rules = array(
-        "email" => ["required", "email", "unique:users,email"],
+        "email" => ["required", "email"],
         "password" => ["required", "min:6"],
         "first_name" => "required|max:255",
         "last_name" => "required|max:255",
