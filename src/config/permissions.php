124a2530e006515fbbd6a605dfa5e06f60693c19
Jacopo <beschi.jacopo@gmail.com>
2014-04-07 16:48:51 +0200

    modificato gruppo permessi

--- src/config/permissions.php
+++ src/config/permissions.php
@@ -2,6 +2,6 @@
 
 return [
     "edit_profile" => ["_profile", "_admin"],
-    "profile_notification_group" => "admin",
+    "profile_notification_group" => "mail_ordini",
     "admin_area" => ['_admin']
 ];
