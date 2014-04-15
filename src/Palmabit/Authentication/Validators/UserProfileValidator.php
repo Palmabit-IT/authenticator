8662d087f6d1576942c7a5aac3f8a229d837e44b
Jacopo <beschi.jacopo@gmail.com>
2014-04-15 14:34:43 +0200

    in profilo pass nn obbligatoria

--- src/Palmabit/Authentication/Validators/UserProfileValidator.php
+++ src/Palmabit/Authentication/Validators/UserProfileValidator.php
@@ -8,6 +8,6 @@ class UserProfileValidator extends OverrideConnectionValidator
     protected static $rules = array(
         "first_name" => "max:50|required",
         "last_name" => "max:50|required",
-        "password" => "max:20|min:6|required"
+        "password" => "max:20|min:6"
     );
 }
