b69163070683d4b5b01ff7bd26fbeb058f862d1d
Jacopo <beschi.jacopo@gmail.com>
2014-05-21 09:29:07 +0200

    fix menu e permessi utenti default attivi solo quelli

--- src/config/menu.php
+++ src/config/menu.php
@@ -17,18 +17,18 @@ return [
             /**
              * Name of the permission needed to view the button
              */
-            "permissions" => ["_admin"]
+            "permissions" => ["_superadmin"]
         ],
-        [
-            "name" => "Prodotti",
-            "route" => "products",
-            "link" => URL::route('products.lists'),
-            "permissions" => ["_admin"]
-        ],
-        [
-            "name" => "Categorie",
-            "route" => "category",
-            "link" => URL::route('category.lists'),
-            "permissions" => ["_admin"]
-        ]
+//        [
+//            "name" => "Prodotti",
+//            "route" => "products",
+//            "link" => URL::route('products.lists'),
+//            "permissions" => ["_admin"]
+//        ],
+//        [
+//            "name" => "Categorie",
+//            "route" => "category",
+//            "link" => URL::route('category.lists'),
+//            "permissions" => ["_admin"]
+//        ]
 ];
--- src/config/permissions.php
+++ src/config/permissions.php
@@ -4,11 +4,11 @@ return [
     /**
      * Permission requirements to edit an user profile
      */
-    "edit_profile" => ["_profile", "_admin"],
+    "edit_profile" => ["_profile", "_superadmin"],
     /**
      * Group of users that gets notifications
      */
-    "profile_notification_group" => "admin",
+    "profile_notification_group" => "superadmin",
     /**
      * Permission requirements to access the admin area
      */
