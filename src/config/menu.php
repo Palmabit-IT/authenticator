29300f5e1b559c32b8fdc0f74e6c2d88f1410ced
Jacopo <beschi.jacopo@gmail.com>
2014-05-21 09:37:57 +0200

    fix bug menu doppio

--- src/config/menu.php
+++ src/config/menu.php
@@ -1,34 +1,36 @@
 <?php
 
 return [
+    "list" => [
         [
             /**
              * Name of the menu field in the panel
              */
-            "name" => "Utenti",
+            "name"        => "Utenti",
             /**
              * Name of the route associated with the button
              */
-            "route" => "users",
+            "route"       => "users",
             /**
              * Effective link url
              */
-            "link" => URL::route('users.list'),
+            "link"        => URL::route('users.list'),
             /**
              * Name of the permission needed to view the button
              */
             "permissions" => ["_superadmin"]
         ],
-//        [
-//            "name" => "Prodotti",
-//            "route" => "products",
-//            "link" => URL::route('products.lists'),
-//            "permissions" => ["_admin"]
-//        ],
-//        [
-//            "name" => "Categorie",
-//            "route" => "category",
-//            "link" => URL::route('category.lists'),
-//            "permissions" => ["_admin"]
-//        ]
+        //        [
+        //            "name" => "Prodotti",
+        //            "route" => "products",
+        //            "link" => URL::route('products.lists'),
+        //            "permissions" => ["_admin"]
+        //        ],
+        //        [
+        //            "name" => "Categorie",
+        //            "route" => "category",
+        //            "link" => URL::route('category.lists'),
+        //            "permissions" => ["_admin"]
+        //        ]
+    ]
 ];
