fa5238e3958d857f316179ad71995612000ce38b
Jacopo <beschi.jacopo@gmail.com>
2014-05-23 10:21:27 +0200

    fix msg password recovery

--- src/views/auth/confirmation.blade.php
+++ src/views/auth/confirmation.blade.php
@@ -6,11 +6,11 @@
           <div class="col-md-4 col-md-offset-4">
               <div class="login-panel panel panel-success">
                   <div class="panel-heading">
-                      <h3 class="panel-title">Conferma modifica password.</h3>
+                      <h3 class="panel-title">Richiesta modifica password.</h3>
                   </div>
                   <div class="panel-body">
-                    <h1>Password modificata con successo</h1>
-                    <p class="error-para">La tua password è stata modificata con successo.<br> Grazie</p>
+                    <h1>Richiesta password inoltrata</h1>
+                    <p class="error-para">Ti è stata inviata un'email di conferma per modificare la tua password.<br> Grazie</p>
                     <br>
                     <a href="/" class="btn btn-lg btn-success btn-block">Torna al sito</a>
                   </div>
