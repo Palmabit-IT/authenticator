873f0521610245f9611453a60ede4cd905f41a6a
Jacopo <beschi.jacopo@gmail.com>
2014-08-18 10:52:24 +0200

    fix user search populate

--- src/views/user/search.blade.php
+++ src/views/user/search.blade.php
@@ -4,11 +4,22 @@
     </div>
     <div class="panel-body">
         {{Form::open(['action' => 'Palmabit\Authentication\Controllers\UserController@getList','method' => 'get'])}}
-        {{FormField::email(['label' => 'email'])}}
-        {{FormField::first_name(['label' => 'Nome:'])}}
-        {{FormField::last_name(['label' => 'Cognome:'])}}
-        {{FormField::billing_address_zip(['label' => 'Cap pagamento:'])}}
-        {{FormField::code(['label' => 'Codice utente:'])}}
+
+        {{Form::label('email', 'Email: ')}}
+        {{Form::text('email', Input::get('email'),['class' => 'form-control'])}}
+
+        {{Form::label('first_name', 'Nome: ')}}
+        {{Form::text('first_name', Input::get('first_name'),['class' => 'form-control'])}}
+
+        {{Form::label('last_name', 'Cognome: ')}}
+        {{Form::text('last_name', Input::get('last_name'),['class' => 'form-control'])}}
+
+        {{Form::label('billing_address_zip', 'Cap pagamento: ')}}
+        {{Form::text('billing_address_zip', Input::get('billing_address_zip'),['class' => 'form-control'])}}
+
+        {{Form::label('code', 'Codice utente:')}}
+        {{Form::text('code', Input::get('code'),['class' => 'form-control'])}}
+
         <div class="form-group">
             {{Form::label('activated', 'Attivo: ')}}
             {{Form::select('activated', ['' => '', 1 => 'Sì', 0 => 'No'], Input::get('activated',''), ["class" => "form-control"])}}
